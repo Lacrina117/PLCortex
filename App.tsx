52054855df9ac9574574928dea25631a4bdceac1
Lacrina117 <lacrina.117@gmail.com>
2025-10-03 20:28:17 -0700

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -95,7 +95,7 @@ const App: React.FC = () => {
                 : "flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8";
 
             return (
-                <div className="flex flex-col h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
+                <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
                     <Header currentView={currentView} setView={handleSetView} />
                     <main className={mainClasses}>
                         {renderCurrentView()}
